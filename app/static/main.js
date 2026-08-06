06581c04a0decd76a3b7882970f3d95597bc3e8f
Siraj <sirajmotaung@gmail.com>
2026-08-06 02:28:30 +0200

    Integrate Google Maps and display default map

--- app/static/main.js
+++ app/static/main.js
@@ -1,1 +1,17 @@
-console.log("Application Loaded");
+let map;
+
+function DefaultMap(){
+
+    map = new google.maps.Map(
+        document.getElementById("map"),
+        {
+            zoom:13,
+            center:{
+                lat:-26.1076,
+                lng:28.0567
+            }
+        }
+    );
+}
+
+window.onload = DefaultMap;
