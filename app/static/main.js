0386875ac2b8d951818f15c85fcd739c3f26314e
Siraj <sirajmotaung@gmail.com>
2026-08-06 02:35:07 +0200

    Add search form and API request

--- app/static/main.js
+++ app/static/main.js
@@ -14,4 +14,19 @@ function DefaultMap(){
     );
 }
 
+document.getElementById("search-btn").addEventListener("click", async ()=>{
+
+    const address = document.getElementById("address-input").value;
+
+    const type = document.getElementById("place-type").value;
+
+    const response = await fetch(`/api/nearby?address=${encodeURIComponent(address)}&type=${type}`);
+
+    const data = await response.json();
+
+    console.log(data);
+});
+
+
+
 window.onload = DefaultMap;
