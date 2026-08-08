df1694b4e8048e89a35b072faa02a0a0a36720bf
Siraj <sirajmotaung@gmail.com>
2026-08-08 02:33:15 +0200

    configured logging

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 
@@ -5,3 +6,16 @@ class Config:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+    LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+
+
+    @classmethod
+    def setup_logging(cls):
+        logging.basicConfig(level=cls.LOG_LEVEL,
+                            format=cls.LOG_FORMAT,
+                            force=True
+                            )
+
+        logger = logging.getLogger(__name__)
+        logging.info("logging initialized at %s level", cls.LOG_LEVEL)
