f708a4fb2d00b46157b1f7f218cfbf8b38cd3c63
Siraj <sirajmotaung@gmail.com>
2026-08-08 22:31:15 +0200

    Application configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,28 @@
 from flask import Flask
 import requests
 
-from config import Config
+from app.config import Config
 from app.error_handlers import handle_app_error, handle_unexpected_error
-from.routes.places import places_bp
+from .routes.places import places_bp
 from app.services.google_map_service import GoogleMapService
-from utils.errors import AppError
+from app.utils.errors import AppError
 
 
 def create_app():
 
     app = Flask(__name__)
 
+    Config.setup_logging()
+
     # Load configuration
     app.config.from_object(Config)
+    
 
     # Create HTTP session
     session = requests.Session()
 
     # Create application dependencies
-    google_service = GoogleMapService(api_key=app.config["GOOGLE_API_KEY"], session=session)
+    google_service = GoogleMapService(api_key=app.config["GOOGLE_API_KEY"], sessions=session)
 
     app.extensions["google_maps_service"] = google_service
 
