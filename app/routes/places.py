ec7962cb0bd2b383e21c530d7eb3d37f0bf20085
Siraj <sirajmotaung@gmail.com>
2026-08-10 03:31:25 +0200

    added more return data to nearby endpoint

--- app/routes/places.py
+++ app/routes/places.py
@@ -11,6 +11,10 @@ places_bp = Blueprint("places", __name__)
 CONFIG = Config()
 GOOGLE_SERVICE = GoogleMapService(CONFIG.GOOGLE_API_KEY, requests.Session())
 
+@places_bp.route("/")
+def index():
+    return render_template("index.html", google_api_key=CONFIG.GOOGLE_API_KEY)
+
 @places_bp.route("/api/nearby", methods=["GET"])
 def nearby_search():
 
@@ -19,8 +23,12 @@ def nearby_search():
 
     coordinates = GOOGLE_SERVICE.geocode(address)
 
-    places = GOOGLE_SERVICE.nearby_search(coordinates["latitude"], coordinates["longitude"], place_type)
+    places = GOOGLE_SERVICE.nearby_search(coordinates["latitude"], coordinates["longitude"], place_type,radius=20000)
 
     return jsonify({
-        "places": places
+        "location": {
+            "lat": coordinates["latitude"],
+            "lng": coordinates["longitude"]
+        },
+        "places": places["places"]
     }), 200
